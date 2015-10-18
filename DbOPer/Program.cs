619f369d189b46a38230d1ca49a0291a6028a308
Chen Weiqing <kevincwq@gmail.com>
2015-10-18 10:35:23 +0800

    add a db operation project

---
+++ DbOPer/Program.cs
@@ -0,0 +1,19 @@
+﻿//
+//  Program.cs
+//
+//  Chen Weiqing <kevincwq@gmail.com>
+//
+//  Copyright (c) 2015 All Right Reserved.
+//
+using System;
+
+namespace DbOPer
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
